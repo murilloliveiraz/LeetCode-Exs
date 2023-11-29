b6b5345f205a57c001fdad9cc31d17fcfdeb492d
Murillo Oliveira <murillo1047@gmail.com>
2023-11-29 16:47:39 -0300

    feat: sleep, array reduce and counter exercises

---
+++ easy/2621-Sleep.js
@@ -0,0 +1,21 @@
+//Given a positive integer millis, write an asynchronous function that sleeps for millis milliseconds. It can resolve any value. 
+
+// Example 1:
+
+// Input: millis = 100
+// Output: 100
+// Explanation: It should return a promise that resolves after 100ms.
+// let t = Date.now();
+// sleep(100).then(() => {
+//   console.log(Date.now() - t); // 100
+// });
+// Example 2:
+
+// Input: millis = 200
+// Output: 200
+// Explanation: It should return a promise that resolves after 200ms.
+
+const sleep = async (millis) => new Promise((r) => setTimeout(r, millis));
+  
+let t = Date.now()
+sleep(200).then(() => console.log(Date.now() - t)) // 100
